d6311d39710e412cff331782a420fb3919da7d3e
domeeno <dominicflocea.dev@gmail.com>
2024-10-07 21:25:47 +0300

    fix kotlin sonarlint git leakage. enhance main function

--- lab-papers-please/kotlin-classification/src/main/kotlin/Main.kt
+++ lab-papers-please/kotlin-classification/src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 package oop.practice
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
 import java.io.File
 
 fun main() {
@@ -8,20 +11,20 @@ fun main() {
   val input = objectMapper.readTree(File("src/main/resources/test-input.json"))
   val data = input["data"]
 
-  val starWars = mutableListOf<String>()
-  val hitchHiker = mutableListOf<String>()
-  val rings = mutableListOf<String>()
-  val marvel = mutableListOf<String>()
+  val starWars = Universe("starwars", mutableListOf())
+  val hitchHiker = Universe("hitchHiker", mutableListOf())
+  val rings = Universe("rings", mutableListOf())
+  val marvel = Universe("marvel", mutableListOf())
 
   data.forEach {
     val node = it
     println(node)
 
     when (readlnOrNull()) {
-      "1" -> starWars.add(node.asText())
-      "2" -> hitchHiker.add(node.asText())
-      "3" -> rings.add(node.asText())
-      "4" -> marvel.add(node.asText())
+      "1" -> starWars.individuals.add(node)
+      "2" -> hitchHiker.individuals.add(node)
+      "3" -> rings.individuals.add(node)
+      "4" -> marvel.individuals.add(node)
     }
   }
 
@@ -30,3 +33,8 @@ fun main() {
   File("src/main/resources/output/rings.json").writeText(objectMapper.writeValueAsString(rings))
   File("src/main/resources/output/marvel.json").writeText(objectMapper.writeValueAsString(starWars))
 }
+
+data class Universe(
+  val name: String,
+  val individuals: MutableList<JsonNode>
+)
