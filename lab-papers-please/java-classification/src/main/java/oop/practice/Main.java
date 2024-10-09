3dc729b251ca8983f0854b74458dbed75d78299f
caramisca <caramisca2@gmail.com>
2024-10-09 22:57:18 +0300

    Day 1 - First commit, created a simple class that identifies from test-input.json , traits and prints them along with the corresponding ids

--- lab-papers-please/java-classification/src/main/java/oop/practice/Main.java
+++ lab-papers-please/java-classification/src/main/java/oop/practice/Main.java
@@ -12,8 +12,16 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
-    File inputFile = new File("src/main/resources/test-input.json");
+    File inputFile = new File("./lab-papers-please/java-classification/src/main/resources/test-input.json");
     JsonNode data = mapper.readTree(inputFile).get("data");
+
+    String filePath = "./lab-papers-please/java-classification/src/main/resources/test-input.json";
+    CharacterTraitsPrinter printer = new CharacterTraitsPrinter();
+    try {
+      printer.printCharacterTraits(filePath);
+    } catch (IOException e) {
+      System.out.println("An error occurred while processing the file: " + e.getMessage());
+    }
 
     Universe starWars = new Universe("starWars", new ArrayList<>());
     Universe hitchhikers = new Universe("hitchHiker", new ArrayList<>());
