77d0063206d3e90f18afd82b4d86a722c087a751
Elena <lenaromasenco@gmail.com>
2024-10-13 16:52:20 +0300

    Revert "Created Universe and Alien classes"
    This reverts commit 5c24f44c4c8b955ad9dc39d04e47f4c4452a0360.

--- lab-papers-please/csharp-classification/Core/Universe.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace csharp_classification.Core
-{
-    public class Universe
-    {
-       public string Name { get; set; } = string.Empty;
-       public List<Alien> Aliens { get; set; } = new List<Alien>();
-
-    }
-}
