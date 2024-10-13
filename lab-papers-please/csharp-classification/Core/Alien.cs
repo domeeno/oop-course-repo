5c24f44c4c8b955ad9dc39d04e47f4c4452a0360
Elena <lenaromasenco@gmail.com>
2024-10-13 15:50:38 +0300

    Created Universe and Alien classes

---
+++ lab-papers-please/csharp-classification/Core/Alien.cs
@@ -0,0 +1,13 @@
+﻿
+namespace csharp_classification.Core
+{
+    public class Alien
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public string? Planet { get; set; } = string.Empty;
+        public bool? IsHumanoid { get; set; }
+        public List<string>? Traits { get; set; } = new List<string>();
+        public double? Age { get; set; }
+    }
+}
---
+++ lab-papers-please/csharp-classification/Core/Universe.cs
@@ -0,0 +1,9 @@
+﻿namespace csharp_classification.Core
+{
+    public class Universe
+    {
+       public string Name { get; set; } = string.Empty;
+       public List<Alien> Aliens { get; set; } = new List<Alien>();
+
+    }
+}
